a464c9c20d395e337368c8c41d88b3fce24711b7
toni <toni.salvado.rubio@gmail.com>
2023-03-07 13:06:44 +0100

    freat: add errorController & custom ProblDetFact

--- PasswordRecovery.Api/Program.cs
+++ PasswordRecovery.Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using PasswordRecovery.Api.Common.Errors;
 using PasswordRecovery.Application;
 using PasswordRecovery.Infrastructure;
 
@@ -10,10 +12,14 @@ var builder = WebApplication.CreateBuilder(args);
     .AddInfrastructure(config);
 
     builder.Services.AddControllers();
+
+    // Overwrite Problem Details Factory with Custom Problem Details Factory from global error handling 
+    builder.Services.AddSingleton<ProblemDetailsFactory, CustomProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
